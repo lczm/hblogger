e6ab7509fdcf4c8d64d0a336d8d94f84e63d118e
lczm <zemingchua@gmail.com>
2023-03-22 16:04:54 +0800

    06-escaping_characters

--- app/Html.hs
+++ app/Html.hs
@@ -26,6 +26,19 @@ render html =
     case html of
         Html str -> str
 
+escape :: String -> String
+escape =
+    let 
+        escapeChar c =
+            case c of
+                '<' -> "&lt;"
+                '>' -> "&gt;"
+                '&' -> "&amp;"
+                '"' -> "&quot;"
+                '\'' -> "&#39;"
+                _ -> [c]
+    in concat . map escapeChar
+
 append_ :: Structure -> Structure -> Structure
 append_ a b = 
     Structure (getStructureString a <> getStructureString b)
@@ -38,13 +51,15 @@ html_ :: Title -> Structure -> Html
 html_ title content =
     Html
         ( el "html"
-            ( el "head" ( el "title" title )
+            ( el "head" ( el "title" (escape title ))
                 <> el "body" (getStructureString content)
             )
         )
 
+-- This runs the input through escape first
 p_ :: String -> Structure
-p_ = Structure . el "p"
+p_ = Structure . el "p" . escape
 
+-- This runs the input through escape first
 h1_ :: String -> Structure
-h1_ = Structure . el "h1"
+h1_ = Structure . el "h1" . escape
