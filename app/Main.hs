0636a574db514b37d3e60eea6eb8ae1618b189c2
lczm <zemingchua@gmail.com>
2023-03-22 14:18:49 +0800

    04-safer_construction

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,55 @@
 module Main where
+
+newtype Html = Html String
+newtype Structure = Structure String
+
+type Title = String
+
+getStructureString :: Structure -> String
+getStructureString (Structure str) = str
+-- getStructureString content =
+--     case content of
+--         Structure str -> str
+
+render :: Html -> String
+render html =
+    case html of
+        Html str -> str
+
+append_ :: Structure -> Structure -> Structure
+append_ a b = 
+    Structure (getStructureString a <> getStructureString b)
+
 el :: String -> String -> String
 el tag content =
     "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-html_ :: String -> String
-html_ = el "html"
+html_ :: Title -> Structure -> Html
+html_ title content =
+    Html
+        ( el "html"
+            ( el "head" ( el "title" title )
+                <> el "body" (getStructureString content)
+            )
+        )
 
-body_ :: String -> String
-body_ = el "body"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-head_ :: String -> String
-head_ = el "head"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
-title_ :: String -> String
-title_ = el "title"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
-
--- Try out writing using lambdas
-h2 = (\h2 -> el h2)
-
-makeHtml :: String -> String -> String
-makeHtml title body = html_ (head_ (title_ title) <> body_ (h1_ (p_ body)))
-
-myhtml :: String
-myhtml = makeHtml "My page title" "Hello world!"
+myhtml :: Html
+myhtml = 
+    html_
+        "My title"
+        ( append_
+            ( h1_ "Heading" )
+            ( append_
+                ( p_ "Paragraph #1" )
+                ( p_ "Paragraph #2" )
+            )
+        )
 
 main :: IO ()
-main = putStrLn myhtml
+main = putStrLn (render myhtml)
