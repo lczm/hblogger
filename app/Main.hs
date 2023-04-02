e638246e4c830b073ad8b8fed38a05340cd4d6f1
lczm <zemingchua@gmail.com>
2023-04-02 10:51:29 +0800

    05-glue/02-io

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,52 @@
 module Main where
 import Html
 import Markup
+import Convert (convert)
+import System.Directory (doesFileExist)
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        -- No program arguments: reading from stdin and writing to stdout
+        [] -> do
+            content <- getContents
+            putStrLn (process "Empty title" content)
+        -- With input and output file paths as program arguments
+        [input, output] -> do
+            content <- readFile input
+            exists <- doesFileExist output
+            let
+                writeResult = writeFile output (process input content)
+            if exists
+                then whenIO confirm writeResult
+                else writeResult
+        -- Any other kind of arguments
+        _ ->
+            putStrLn "Usage: {binary} [-- <input-file> <output-file>]"
+
+-- Parse a document to markup, convert to HTML then render HTML to string
+process :: Html.Title -> String -> String
+process title = Html.render . convert title . Markup.parse
+
+confirm :: IO Bool
+confirm = do
+    putStrLn "Are you sure? (y/n)"
+    answer <- getLine
+    case answer of 
+        "y" -> pure True
+        "n" -> pure False
+        _ -> do 
+            putStrLn "Invalid response. use y or n"
+            confirm
+
+whenIO :: IO Bool -> IO () -> IO ()
+whenIO cond action = do
+    result <- cond
+    if result
+        then action
+        else pure ()
 
 print :: Show a => a -> IO ()
 print = putStrLn . show
@@ -16,6 +62,3 @@ myhtml =
                 ( p_ "Paragraph #2" )
             )
         )
-
-main :: IO ()
-main = putStrLn (render myhtml)
